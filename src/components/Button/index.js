a234b555dce3ee9584d462dbe145b7a2ab6d2ac4
Petrashka <piotrazsko@gmail.com>
2019-06-16 22:21:48 +0300

    add styled button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,17 @@
 import React from "react";
+import ButtonMaterial from "@material-ui/core/Button";
 import styles from "./styles.module.scss";
 console.log(styles);
-const Button = () => <div className={styles.button}>test_test</div>;
+const Button = ({ children, onClick, ...props }) => (
+  <ButtonMaterial
+    variant="contained"
+    color="primary"
+    onClick={onClick}
+    {...props}
+    className={styles.button}
+  >
+    {children}
+  </ButtonMaterial>
+);
 
 export default Button;
