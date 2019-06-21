417be646f9fca466b65f587cb9fabb884fd80e45
dmitriy <kolobokde@mail.ru>
2019-06-21 10:14:21 +0300

    delete fileindex2.js

--- src/components/Rating/rating.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import rating-style from "./rating-style.css"
-//
-// <div class="rating">
-//   <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-// </div>
-//
-// export default rating;
