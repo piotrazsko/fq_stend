f97bf30d08b4a917d426f2a77342b3b23d1d66ea
dmitriy <kolobokde@mail.ru>
2019-06-22 09:18:37 +0300

    add FixedContainer

--- src/components/InputRating/index.js
+++ src/components/InputRating/index.js
@@ -1,29 +1,23 @@
-
-import React from "react";
-import PropTypes from "prop-types";
-import RatingComponent from "react-rating";
-import emptyStar from "../../static/images/emptyStar.svg";
-import fullStar from "../../static/images/fullStar.svg";
-
+import React from 'react'
+import PropTypes from 'prop-types'
+import RatingComponent from 'react-rating'
+import emptyStar from '../../static/images/emptyStar.svg'
+import fullStar from '../../static/images/fullStar.svg'
 
 const InputRating = ({ rating = 0, className, onClick }) => (
-  <RatingComponent
-    initialRating={parseFloat(rating)}
-    fractions={1}
-    onClick={rating=>onClick(rating)}
-    emptySymbol={
-      <img className={className} src={emptyStar} alt="пустая звезда" />
-    }
-    fullSymbol={
-      <img className={className} src={fullStar} alt="заполненная звезда" />
-    }
-  />
-);
+	<RatingComponent
+		initialRating={parseFloat(rating)}
+		fractions={1}
+		onClick={rating => onClick(rating)}
+		emptySymbol={<img className={className} src={emptyStar} alt="пустая звезда" />}
+		fullSymbol={<img className={className} src={fullStar} alt="заполненная звезда" />}
+	/>
+)
 
 InputRating.propTypes = {
-  rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  className: PropTypes.string,
-onClick:  PropTypes.func
-};
+	rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+	className: PropTypes.string,
+	onClick: PropTypes.func,
+}
 
-export default InputRating;
+export default InputRating
