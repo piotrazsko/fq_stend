04678c7d8f5e34a959cbfc13698793c86c2e7d0a
Петрашко Сергей Викторович <siarhei.petrashka@rednavis.com>
2019-09-16 09:20:20 +0300

    build dist

---
+++ dist/precache-manifest.3a5a31e410efc8e2967c1f8a4a62b991.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666e418bf4c3e3a00561",
+    "url": "build/2.666e418b.js"
+  },
+  {
+    "revision": "761e201cce018994438e",
+    "url": "build/bundle.761e201c.js"
+  },
+  {
+    "revision": "5024bf8b913910ab26f7",
+    "url": "build/main.5024bf8b.js"
+  },
+  {
+    "revision": "0d2810cbe622973de700b0e4a4052077",
+    "url": "index.html"
+  },
+  {
+    "revision": "5024bf8b913910ab26f7",
+    "url": "static/css/main.8f3532a6.chunk.css"
+  },
+  {
+    "revision": "097295b898a7e7c4ead484051257ba1e",
+    "url": "static/media/appStore.097295b8.png"
+  },
+  {
+    "revision": "3700bbdca09059693027c6046a335a1a",
+    "url": "static/media/avatar_icon.3700bbdc.svg"
+  },
+  {
+    "revision": "faf4540f9acd4ef15a9ac352fb1dc68d",
+    "url": "static/media/emptyStar.faf4540f.svg"
+  },
+  {
+    "revision": "d09a9f0463a6f7efecd7ac8b223e7294",
+    "url": "static/media/fullStar.d09a9f04.svg"
+  },
+  {
+    "revision": "ad720b31b694ade3b53cf489497c08e9",
+    "url": "static/media/googlePlay.ad720b31.png"
+  },
+  {
+    "revision": "d09a9f0463a6f7efecd7ac8b223e7294",
+    "url": "static/media/xsmallstar.d09a9f04.svg"
+  }
+]);
