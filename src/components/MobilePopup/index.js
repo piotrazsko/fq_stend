453b7ba08d45cab92a71e6e4c9030bc09aa0675d
JuliaMaslovskaya <iuliia.maslovskaia.99@gmail.com>
2019-09-20 04:29:22 +0300

    created mobilePopup

---
+++ src/components/MobilePopup/index.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import PopupBackground from '../PopupBackground'
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import MaskedInput from 'react-text-mask';
+import image from "../../static/images/Group 3.svg";
+import InputLabel from '@material-ui/core/InputLabel';
+import styles from './style.module.scss'
+
+
+function TextMaskCustom(props) {
+  const { inputRef, ...other } = props;
+
+  return (
+    <MaskedInput
+      {...other}
+      ref={ref => {
+        inputRef(ref ? ref.inputElement : null);
+      }}
+      mask={['(',/\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, ' ', /\d/, /\d/,' ', /\d/, /\d/]}
+      placeholderChar={'\u2000'}
+    />
+  );
+}
+
+export default function MobilePopup() {
+
+
+    const [values, setValues] = React.useState({
+     textmask: '(  )       ',
+      numberformat: '1320',
+    });
+
+    const handleChange = name => event => {
+      setValues({
+        ...values,
+        [name]: event.target.value,
+      });
+    };
+
+  return (
+    	<PopupBackground visible >
+       <Grid container className={styles.container} >
+            <Paper className={styles.paper}>
+            <div >
+              <img className={styles.img} alt="logo" src={image} />
+            </div>
+            <Grid item>
+            <div className={styles.registration}>Регистрация</div>
+            <div className={styles.text}>Введите ваш номер телефона</div>
+            </Grid>
+            <div className={styles.phoneContainer}>
+            <Grid  container spacing={1} alignItems="flex-end">
+            <Grid item>
+            <InputLabel className={styles.label}>+375</InputLabel>
+          </Grid>
+            <Grid item>
+            <div className={styles.input}>
+          <Input
+           className={styles.numbers}
+            value={values.textmask}
+            onChange={handleChange('textmask')}
+            id="formatted-text-mask-input"
+            inputComponent={TextMaskCustom}
+          />
+             </div></Grid>
+            </Grid></div>
+            <Grid item>
+            <div className={styles.button}>место для кнопки</div>
+            </Grid>
+            </Paper>
+          </Grid>
+    </PopupBackground>
+  );
+}
