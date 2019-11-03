26d0fabe747082e5efe95b82fe857847ddf9afe9
Petrashka <piotrazsko@gmail.com>
2019-11-03 18:14:08 +0300

    added phoneInput

--- src/components/StoresBox/index.js
+++ src/components/StoresBox/index.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styles from './styles.module.scss'
-import Avatar from '../Avatar'
-import appStore from '../../static/images/stores/appStore.png'
-import googlePlay from '../../static/images/stores/googlePlay.png'
-import Button from '@material-ui/core/Button'
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './styles.module.scss';
+import Avatar from '../Avatar';
+import appStore from '../../static/images/stores/appStore.png';
+import googlePlay from '../../static/images/stores/googlePlay.png';
+import Button from '@material-ui/core/Button';
 
 const StoresBox = ({ onClickgoogle = () => {}, onClick }) => {
 	return (
 		<div className={styles.container}>
 			<Button
 				onClick={() => {
-					alert('Скоро в AppStore ')
+					alert('Скоро в AppStore ');
 				}}
 			>
 				<Avatar imageUrl={appStore} className={styles.appStore} />
 			</Button>
-			<Button onClick={onClickgoogle} target="_blank" href="https://play.google.com/store">
+			<Button onClick={onClickgoogle}>
 				<Avatar imageUrl={googlePlay} className={styles.googlePlay} />
 			</Button>
 		</div>
-	)
-}
+	);
+};
 
 StoresBox.propTypes = {
-	onClickgoogle: PropTypes.string,
+	onClickgoogle: PropTypes.func,
 	onClick: PropTypes.func,
-}
+};
 
-export default StoresBox
+export default StoresBox;
