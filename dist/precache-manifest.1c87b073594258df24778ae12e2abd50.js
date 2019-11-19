37249a3c10c301729139f948fa5835bc73d11175
Serj <serj432@yandex.ru>
2019-11-19 16:13:18 +0300

    fixed bug with safary*i

---
+++ dist/precache-manifest.1c87b073594258df24778ae12e2abd50.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45e32df01e17b83fab3",
+    "url": "build/2.a45e32df.js"
+  },
+  {
+    "revision": "016cd79aefb974ae89bb",
+    "url": "build/bundle.016cd79a.js"
+  },
+  {
+    "revision": "6380810afe22fffa4a7a",
+    "url": "build/main.6380810a.js"
+  },
+  {
+    "revision": "30042c5e7eeb4844b9d1f6b0682ee9ce",
+    "url": "index.html"
+  },
+  {
+    "revision": "a45e32df01e17b83fab3",
+    "url": "static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "6380810afe22fffa4a7a",
+    "url": "static/css/main.1788542e.chunk.css"
+  },
+  {
+    "revision": "097295b898a7e7c4ead484051257ba1e",
+    "url": "static/media/appStore.097295b8.png"
+  },
+  {
+    "revision": "3700bbdca09059693027c6046a335a1a",
+    "url": "static/media/avatar_icon.3700bbdc.svg"
+  },
+  {
+    "revision": "faf4540f9acd4ef15a9ac352fb1dc68d",
+    "url": "static/media/emptyStar.faf4540f.svg"
+  },
+  {
+    "revision": "d09a9f0463a6f7efecd7ac8b223e7294",
+    "url": "static/media/fullStar.d09a9f04.svg"
+  },
+  {
+    "revision": "ad720b31b694ade3b53cf489497c08e9",
+    "url": "static/media/googlePlay.ad720b31.png"
+  },
+  {
+    "revision": "d09a9f0463a6f7efecd7ac8b223e7294",
+    "url": "static/media/xsmallstar.d09a9f04.svg"
+  }
+]);
--- es/components/Calendar/utils/getHoursFromEvents.js
+++ es/components/Calendar/utils/getHoursFromEvents.js
@@ -1,10 +1,11 @@
+import moment from 'moment';
 export default function getHoursFromEvents() {
   var events = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : [];
   var result = [];
 
   for (var i = 0; i < events.length; i++) {
     if ('date' in events[i] && 'duration' in events[i]) {
-      var time = new Date(events[i].date);
+      var time = new Date(moment(events[i].date));
       var startHour = time.getHours() * 2 + time.getMinutes() * 2 / 60;
       var hours = Math.ceil(events[i].duration / 60 * 2);
 
@@ -16,5 +17,6 @@ export default function getHoursFromEvents() {
     }
   }
 
+  console.log(result);
   return result;
 }
