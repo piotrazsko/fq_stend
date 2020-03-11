ffea6e5cf407fb5ef20102a840e9c895b13d48fd
Serj <serj432@yandex.ru>
2020-03-11 12:19:27 +0300

    removed external algotims

--- es/components/WorkingTimeSelect/index.js
+++ es/components/WorkingTimeSelect/index.js
@@ -19,7 +19,6 @@ import PropTypes from 'prop-types';
 import Grid from '../Grid';
 import Cell from './Cell';
 import Days from './Days';
-import { preppareDataforWorkTime, recoveryDataForWorkTime } from '../../helpers/calendar';
 var style = {
   "title": "style-module_fq_title___tFfDD",
   "resultContainer": "style-module_fq_resultContainer___10tCz",
@@ -38,17 +37,15 @@ var WorkingTimeSelect = function WorkingTimeSelect(_ref) {
       _ref$workingTime = _ref.workingTime,
       workingTime = _ref$workingTime === void 0 ? [] : _ref$workingTime,
       _ref$isMobile = _ref.isMobile,
-      isMobile = _ref$isMobile === void 0 ? false : _ref$isMobile,
-      _ref$usePreparing = _ref.usePreparing,
-      usePreparing = _ref$usePreparing === void 0 ? true : _ref$usePreparing;
+      isMobile = _ref$isMobile === void 0 ? false : _ref$isMobile;
 
-  var _React$useState = React.useState(_toConsumableArray(usePreparing ? recoveryDataForWorkTime(workingTime) : workingTime)),
+  var _React$useState = React.useState(_toConsumableArray(workingTime)),
       _React$useState2 = _slicedToArray(_React$useState, 2),
       selectedTime = _React$useState2[0],
       selectTime = _React$useState2[1];
 
   React.useEffect(function () {
-    onChange(_toConsumableArray(usePreparing ? preppareDataforWorkTime(selectedTime) : selectedTime));
+    onChange(_toConsumableArray(selectedTime));
   }, [selectedTime]);
   React.useEffect(function () {
     if (workingTime.length !== selectedTime.length || !workingTime.every(function (item) {
@@ -56,7 +53,7 @@ var WorkingTimeSelect = function WorkingTimeSelect(_ref) {
         return i.col == item.col && i.row == item.row;
       });
     })) {
-      selectTime(_toConsumableArray(usePreparing ? recoveryDataForWorkTime(workingTime) : workingTime));
+      selectTime(_toConsumableArray(workingTime));
     }
   }, [workingTime]);
 
@@ -118,8 +115,7 @@ WorkingTimeSelect.propTypes = {
     col: PropTypes.number,
     row: PropTypes.number
   })),
-  isMobile: PropTypes.bool,
-  usePreparing: PropTypes.bool
+  isMobile: PropTypes.bool
 };
 WorkingTimeSelect.defaultProps = {
   workingTime: []
