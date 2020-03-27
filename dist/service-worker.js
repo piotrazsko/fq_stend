1adb82f2b21f7589e2dd1a207f05dc6445bfe1ad
Serj <serj432@yandex.ru>
2020-03-27 13:00:25 +0300

    updated style

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.1a58335d4bef1a2ba1acf7d0735123c0.js"
+  "precache-manifest.9dfdf20793c5ca0d6fc5e37a258bf5d8.js"
 );
 
 self.addEventListener('message', (event) => {
