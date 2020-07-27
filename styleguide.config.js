28832000a964faf38530840f4bad74d56f63592e
Petrashka <piotrazsko@gmail.com>
2020-07-27 18:27:44 +0300

    added  rebuild of svg

--- styleguide.config.js
+++ styleguide.config.js
@@ -20,45 +20,4 @@ module.exports = {
             base: '"Roboto", sans-serif',
         },
     },
-    webpackConfig: {
-        module: {
-            rules: [
-                {
-                    test: /\.js?$/,
-                    exclude: /node_modules/,
-                    loader: 'babel-loader',
-                    options: {
-                        plugins: [
-                            [
-                                require.resolve('babel-plugin-named-asset-import'),
-                                {
-                                    loaderMap: {
-                                        svg: {
-                                            ReactComponent: '@svgr/webpack?-svgo,+ref![path]',
-                                        },
-                                    },
-                                },
-                            ],
-                        ],
-                    },
-                },
-                {
-                    test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-                    loader: 'url-loader',
-                    options: {
-                        limit: 10000,
-                        name: 'static/media/[name].[hash:8].[ext]',
-                    },
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.svg$/,
-                    loader: 'svg-inline-loader',
-                },
-            ],
-        },
-    },
 };
